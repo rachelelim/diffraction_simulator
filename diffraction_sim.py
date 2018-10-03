172c570acd3a1739706bf1c70925141003643a60
rachelelim <relim@andrew.cmu.edu>
2018-10-02 20:18:24 -0400

    fixed coordinate indexing loop, added command line prompt

--- diffraction_sim.py
+++ diffraction_sim.py
@@ -25,6 +25,8 @@ from matplotlib import pyplot as plt
 import time
 from numba import jit
 import h5py
+import os
+import argparse
 
 from hexrd import instrument
 from hexrd.xrd import material
@@ -33,6 +35,8 @@ from hexrd.fitting import peakfunctions as pk
 from hexrd.xrd import rotations as rot
 from hexrd import config
 from hexrd import imageseries
+
+os.chdir('/Users/rachellim/diffraction_simulator')
 
 import diff_sim_util as diffutil #loads all the simulator functions
 
@@ -43,16 +47,32 @@ warnings.filterwarnings('ignore', category=DeprecationWarning)
 
 
 #==============================================================================
-#%% User Input
+#%% Input config file
 #==============================================================================
 
-cfg_filename = 'diff_sim_config_v2.yml'
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+        description="batchfitgrains")
+
+    parser.add_argument('cfg_file',
+                        help="cfg file", type=str)
+
+    args = parser.parse_args()
+    cfg_filename = args.cfg_file
+
+
+else:
+    cfg_filename = '/Users/rachellim/diffraction_simulator/diff_sim_config.yml'
 
 cfg = yaml.load(open(cfg_filename, 'r'))
+working_dir = cfg['working_dir']
 
 #==============================================================================
 #%% Initialization
 #==============================================================================
+
+os.chdir(working_dir)
 
 analysis_name = cfg['analysis_name']
 
@@ -101,7 +121,7 @@ num_bins = cfg['energy']['num_bins'] #odd number is best here, number of bins fo
 
 energy = np.linspace(matl.beamEnergy-FWHM_actual,matl.beamEnergy+FWHM_actual,num=num_bins)
 
-f = np.repeat(10,len(energy))
+f = np.repeat(2,len(energy))
 #a = plt.figure()
 #plt.plot(energy,f,'o-',color='crimson')
 ##plt.bar(energy,f,width = (np.max(energy)-np.min(energy))/num_bins)
@@ -139,25 +159,27 @@ for det_id in instr.detectors:
 for energy_index in range(0,len(energy)):
     start_energy = time.time()
     matl.beamEnergy = energy[energy_index]
-    print('Energy = %.5f keV' %matl.beamEnergy)
+    print('Energy = %.5f keV (%d of %d)' %(matl.beamEnergy, energy_index, num_bins))
     plane_data = matl.planeData
     
     weight = f[energy_index].astype(np.uint16)
-
+    
+    rot_start = time.time()
     # for each detector key, returns valid_ids, valid_hkls, valid_angs, valid_xys, ang_pixel_size
     rotation_series = instr.simulate_rotation_series(plane_data,gparm_list,ome_ranges=ome_ranges)
+    rot_end = time.time()
+    print('\t Rotation Series took %.3f' %(rot_end-rot_start))
 
     for det_id in instr.detectors:
         
         det_X = det_params[det_id]['detector']['pixels']['columns']
         det_Y = det_params[det_id]['detector']['pixels']['rows']      
-   
+        
         XY_pix,ome_angs = diffutil.diffraction_XY(rotation_series,det_id,det_params,ome_ranges,ome_step)
+
         
         for grain in range(0,len(XY_pix)):
-            for coords in range(0,len(XY_pix[grain][0])):
-                det_int[det_id][int(ome_angs[grain][coords]),int(XY_pix[grain][1][coords]),int(XY_pix[grain][0][coords])] += weight
-        
+            det_int[det_id][(ome_angs[grain]).astype(np.uint16),(XY_pix[grain][1]).astype(np.uint16),(XY_pix[grain][0]).astype(np.uint16)] += weight
 #            diff_coord[det_id] = np.vstack([diff_coord[det_id],XY_pix[grain].T])
 #            diff_angs[det_id] = np.hstack([diff_angs[det_id],ome_angs[grain]]) 
    
@@ -165,49 +187,52 @@ for energy_index in range(0,len(energy)):
     print ('\t took %.3f seconds' %(end_energy - start_energy))
 
 end = time.time()
-print ('All detectors took ' + str(end - start) + ' seconds')
+print ('All energies took ' + str(end - start) + ' seconds')
 
 
 #==============================================================================
 #%% Save as FrameCache
 #==============================================================================
 
+start_write = time.time()
 steps = diffutil.make_ome_wedge(ome_start,ome_stop,int((ome_stop-ome_start)/ome_step))
 
 diffutil.save_fc(instr, steps, det_int,analysis_name)
-
+print('Saved to %sfc-%s_det_id.npz' %(working_dir,analysis_name))
+end_write = time.time()
+print('Saving frame-cache took %.3f seconds' %(end_write-start_write))
 
 #%%
-
-x_y_ome = {}
-for det_id in instr.detectors:
-    x_y_ome[det_id] = np.hstack([diff_coord[det_id],np.expand_dims(diff_angs[det_id],axis=1)])
-
-#%% plotting
-for det_id in instr.detectors:
-    pix_size = det_params[det_id]['detector']['pixels']['size']
-    
-    
-    plot_width, plot_height = diffutil.plot_shape(det_params,det_id)
-    
-    XY = diff_coord[det_id]*pix_size #change back to micron coords
-    
-    
-    fig = plt.figure(figsize = (plot_width, plot_height))
-    plt.plot(XY[:,0],XY[:,1],'.',markersize=1)
-    plt.title(det_id,fontsize=20)   
-    plt.show()
-
-
- #%%
-    
-    
-    
-detector_image = plt.figure(figsize = (plot_width*2, plot_height*2))
-#    detector_image = plt.figure()
-ax = plt.axes()
-plt.imshow(det_int[det_id],cmap='inferno')
-plt.colorbar()
-#plt.ylim(bottom = 1900,top= 2000)
-#plt.xlim(left = 850, right = 950)
-plt.title(det_id,fontsize=20)
+#
+#x_y_ome = {}
+#for det_id in instr.detectors:
+#    x_y_ome[det_id] = np.hstack([diff_coord[det_id],np.expand_dims(diff_angs[det_id],axis=1)])
+#
+##%% plotting
+#for det_id in instr.detectors:
+#    pix_size = det_params[det_id]['detector']['pixels']['size']
+#    
+#    
+#    plot_width, plot_height = diffutil.plot_shape(det_params,det_id)
+#    
+#    XY = diff_coord[det_id]*pix_size #change back to micron coords
+#    
+#    
+#    fig = plt.figure(figsize = (plot_width, plot_height))
+#    plt.plot(XY[:,0],XY[:,1],'.',markersize=1)
+#    plt.title(det_id,fontsize=20)   
+#    plt.show()
+#
+#
+# #%%
+#    
+#    
+#    
+#detector_image = plt.figure(figsize = (plot_width*2, plot_height*2))
+##    detector_image = plt.figure()
+#ax = plt.axes()
+#plt.imshow(det_int[det_id],cmap='inferno')
+#plt.colorbar()
+##plt.ylim(bottom = 1900,top= 2000)
+##plt.xlim(left = 850, right = 950)
+#plt.title(det_id,fontsize=20)
